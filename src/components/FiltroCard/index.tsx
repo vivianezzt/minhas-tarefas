27d9ebc668ef4942cc6c4b2509c9903b9725d245
Viviane <vivianeaguiarc@outlook.com>
2024-09-12 16:04:15 -0300

    Parametrizando os filtros

--- src/components/FiltroCard/index.tsx
+++ src/components/FiltroCard/index.tsx
@@ -2,12 +2,14 @@ import * as S from './styles'
 
 export type Props = {
   ativo?: boolean
+  contador: number
+  legenda: string
 }
 
-export const FiltroCard = (props: Props) => (
-  <S.Card ativo={props.ativo}>
-    <S.Contador>3</S.Contador>
-    <S.Label>pendentes</S.Label>
+export const FiltroCard = ({ ativo, contador, legenda }: Props) => (
+  <S.Card ativo={ativo}>
+    <S.Contador>{contador}</S.Contador>
+    <S.Label>{legenda}</S.Label>
   </S.Card>
 )
 
--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -6,12 +6,12 @@ export const BarraLateral = () => (
     <div>
       <S.Campo type="text" placeholder="Buscar" />
       <S.Filtros>
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard ativo />
+        <FiltroCard legenda="pendentes" contador={1} />
+        <FiltroCard legenda="concluidas" contador={2} />
+        <FiltroCard legenda="importantes" contador={3} />
+        <FiltroCard legenda="urgentes" contador={4} />
+        <FiltroCard legenda="normal" contador={5} />
+        <FiltroCard legenda="todas" contador={6} />
       </S.Filtros>
     </div>
   </S.Aside>
